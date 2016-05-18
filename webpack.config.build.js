95e8919798ae7cf35beb6e51f878dc08f413a180
EddieWen-Taiwan <m20021991y@gmail.com>
2016-05-18 23:11:38 +0800

    First commit: Start refactoring

---
+++ webpack.config.build.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: path.resolve(__dirname, 'src', 'app.js'),
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		publicPath: '/',
+		filename: 'bundle.min.js'
+	},
+	module: {
+		loaders: [
+			{ test: /\.js$/, include: /src/, loader: 'babel-loader' },
+			{ test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
+			{ test: /\.(png|jpg)$/, loader: 'file-loader', query: 'name=img/[hash:7].[ext]' }
+		]
+	},
+	resolve: {
+		root: path.resolve(__dirname, 'src'),
+		extensions: ['', '.js'],
+		alias: {}
+	},
+	plugins: [
+		new ExtractTextPlugin('all.min.css')
+	]
+}
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+	entry: path.resolve(__dirname, 'src', 'app.js'),
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		publicPath: 'http://localhost:8080/',
+		filename: 'bundle.min.js'
+	},
+	module: {
+		loaders: [
+			{ test: /\.js$/, include: /src/, loader: 'babel-loader' },
+			{ test: /\.css$/, loader: 'style-loader!css-loader' },
+			{ test: /\.(png|jpg)$/, loader: 'file-loader', query: 'name=img/[hash:7].[ext]' }
+		]
+	},
+	resolve: {
+		root: path.resolve(__dirname, 'src'),
+		extensions: ['', '.js'],
+		alias: {}
+	}
+}
+
