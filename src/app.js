97964fb14b54124d36b166e128b3da5c81b89c1c
EddieWen-Taiwan <m20021991y@gmail.com>
2016-05-19 00:19:42 +0800

    Completed component: first-window

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 require('css/reset');
+
 require('css/first-window');
 require('css/name-wrapper');
+require('css/tag-box');
+
+require('css/helpers');
 
 import $ from 'jquery';
+
+$(document).ready( () => {
+
+});
