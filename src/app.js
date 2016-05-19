5cabf54413ffa2b99997fa8d37e781c82325d05f
EddieWen-Taiwan <m20021991y@gmail.com>
2016-05-19 14:01:40 +0800

    Snippet for calculate how many days

--- src/app.js
+++ src/app.js
@@ -10,11 +10,28 @@ import $ from 'jquery';
 
 $(document).ready( () => {
 
+	/**
+	 * move screen after pressing .arrow
+	 */
 	$('.first-window > .arrow').on( 'click', () => {
-		const windowH = $(window).height();
 		$('body').animate({
-			scrollTop: windowH+300
+			scrollTop: $(window).height()+300
 		}, 1000);
 	});
+
+
+
+	/**
+	 * calculate how many days remains
+	 * delete this after 2016/05/27
+	 */
+	const today = new Date();
+	const FINAL_DAY = new Date('2016/05/27');
+	const MS_PER_DAY = 24 * 60 * 60 * 1000;
+
+	const damnDayLeft = ( FINAL_DAY.getTime() - today.getTime() ) / MS_PER_DAY;
+	if( damnDayLeft > 0 ) {
+		$('.damn').text(`還有 ${Math.floor(damnDayLeft+1)} 天登出`);
+	}
 
 });
